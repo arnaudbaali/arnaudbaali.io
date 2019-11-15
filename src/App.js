fa5167c5869c56b272faf19836c20f67575fabc8
arnaudbaali <arnaud.baali@me.com>
2019-11-15 07:24:07 +0000

    remove section

--- src/App.js
+++ src/App.js
@@ -36,19 +36,6 @@ class App extends Component {
 
           <Experience/>
 
-          <section className="section projects-section">
-            <h2 className="section-title"><FontAwesome name="archive"/>Projects</h2>
-            <div className="intro">
-              <p>You can list your side projects or open source libraries in this section. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et ligula in nunc bibendum fringilla a eu lectus.</p>
-            </div>
-            <div className="item">
-              <span className="project-title"><a href="#hook">Velocity</a></span> - <span className="project-tagline">A responsive website template designed to help startups promote, market and sell their products.</span>
-
-            </div>
-          
-            
-          </section>
-
           <Skills/>
 
         </div>
